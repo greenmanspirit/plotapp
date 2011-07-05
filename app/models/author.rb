9e044cb7be01f314c66c19e605638045c0453481
Adam Hobaugh <adam@river.localdomain>
2011-07-05 17:52:08 -0400

    -Made a very basic author listing

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,8 +8,8 @@ class Author < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :bio, :photo
 
-  has_attached_file :photo, :default_url => "/images/missing.png",
-			    :styles => {:profile => "200x200>"}
+  has_attached_file :photo, :default_url => "/images/:style/missing.png",
+			    :styles => {:profile => "200x200>", :thumb => "150x150>"}
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
--- app/models/character.rb
+++ app/models/character.rb
@@ -14,8 +14,8 @@ class Character < ActiveRecord::Base
 				:greater_than_or_equal_to => 0,
 				:message => "must be 0 or a positive whole number."
 	
-	has_attached_file :photo, :default_url => "/images/missing.png",
-			    :styles => {:profile => "200x200>"}
+	has_attached_file :photo, :default_url => "/images/:style/missing.png",
+			    :styles => {:profile => "200x200>", :thumb => "150x150>"}
   	validates_attachment_size :photo, :less_than => 5.megabytes
   	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
