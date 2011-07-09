b0a40007e7156507116ed804d40b37beaadb19de
Adam Hobaugh <adam@river.localdomain>
2011-07-09 12:18:08 -0400

    -forgot about the profile view for the name change from the last commit -fixed bug where users public stories would show up in random order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   	if author_signed_in? then
-		@authorPublicStories = current_author.stories.ten_public_stories
+		@authorPublicStories = current_author.stories.public_stories(@current_author)
 		@authorPrivateStories = current_author.stories.private_stories
 	end
 	@allPublicStories = Story.public_stories;
--- app/models/story.rb
+++ app/models/story.rb
@@ -6,8 +6,12 @@ class Story < ActiveRecord::Base
 
 	validates_presence_of :title
 
-	def self.public_stories
-		where(["private = ?", false]).all(:order => 'random()')
+	def self.public_stories(user = nil)
+		if(user == nil) then
+			where(["private = ?", false]).all(:order => 'random()')
+		else
+			where(["private = ?", false])
+		end
 	end
 	def self.ten_public_stories
 		where(["private = ?", false]).all(:order => 'random()', :limit => 10)
