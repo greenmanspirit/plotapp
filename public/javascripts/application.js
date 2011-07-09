6b480388ec076631460bdb13d1bef89dfd484a0d
Adam Hobaugh <adam@river.localdomain>
2011-07-09 12:05:56 -0400

    -fixed plotpoint and characters styling for form elements

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,17 +21,17 @@ $(document).ready(function() {
 	});
 	$('#addVirtueLink').click(function() {
 		var num  = $('.virtue').length;
-		$('#virtueList').append(newCharFormElement(num, 'virtue'));
+		$('#virtueListForm').append(newCharFormElement(num, 'virtue'));
 		return false;
 	});
 	$('#addViceLink').click(function() {
 		var num  = $('.vice').length;
-		$('#viceList').append(newCharFormElement(num, 'vice'));
+		$('#viceListForm').append(newCharFormElement(num, 'vice'));
 		return false;
 	});
 	$('#addHabitLink').click(function() {
 		var num  = $('.habit').length;
-		$('#habitList').append(newCharFormElement(num, 'habit'));
+		$('#habitListForm').append(newCharFormElement(num, 'habit'));
 		return false;
 	});
 });
@@ -43,6 +43,6 @@ function newCharFormElement(idNum, charAttribute) {
 	var deleteCheckboxHidden = $('<input>').attr('type','hidden').attr('name',arrayRef+'[_destroy]').attr('value',0);
 	var deleteCheckbox = $('<input>').attr('type','checkbox').attr('name',arrayRef+'[_destroy]').attr('value',1).attr('id',attrStr+'_destroy');
 	var textHidden = $('<input>').attr('type','hidden').attr('name',arrayRef+'[id]').attr('id',attrStr+'id');
-	var text = $('<input>').attr('type','text').attr('name',arrayRef+'[detail]').attr('id',attrStr+'detail').addClass(charAttribute).attr('size',30);
+	var text = $('<input>').attr('type','text').attr('name',arrayRef+'[detail]').attr('id',attrStr+'detail').addClass(charAttribute).addClass('width85').attr('size',30);
 	return li.append(deleteCheckboxHidden).append(deleteCheckbox).append(textHidden).append(text);
 }
